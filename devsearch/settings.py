def5e6eb08d8ecaf2b720a0dee4374ecd00c0519
Allen <98749681+GengarLu@users.noreply.github.com>
2024-03-25 21:56:07 +0800

    Update settings.py

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -31,10 +31,10 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-# ALLOWED_HOSTS = ['django-tutorial-u7ci.onrender.com/']
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['django-tutorial-u7ci.onrender.com/']
 
-# CSRF_TRUSTED_ORIGINS = ['https://django-tutorial-u7ci.onrender.com/']
+CSRF_TRUSTED_ORIGINS = ['https://django-tutorial-u7ci.onrender.com/']
 
 # Application definition
 INSTALLED_APPS = [
